4db71bf148d6f8b754147aac2cb2c62872b53813
zacharyrgonzales-portfolio <zacharyrgonzales.azure@gmail.com>
2024-03-19 01:24:39 -0500

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,6 @@
+import '@/app/ui/global.css';
+import { Inter } from 'next/font/google';
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +8,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="{`${inter.classname} antialiased`}">{children}</body>
     </html>
   );
 }
